35159dcd5b2e64c5dd4303cf6c63ff0b3dc84f1e
amrElzohairy <aelzohairy9@gmail.com>
2025-04-17 14:44:08 +0200

    add banners list

--- lib/features/home/presentation/home_view.dart
+++ lib/features/home/presentation/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -7,7 +8,11 @@ import 'package:fruits_app/core/utils/app_styles.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
-
+  final List<String> banners = const [
+    Assets.imagesSlider1,
+    Assets.imagesSlider2,
+    Assets.imagesSlider3,
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,6 +32,34 @@ class HomeView extends StatelessWidget {
             child: SvgPicture.asset(Assets.imagesBagIcon),
           ),
         ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            CarouselSlider.builder(
+              itemCount: banners.length,
+              itemBuilder:
+                  (BuildContext context, int itemIndex, int pageViewIndex) =>
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(10.r),
+                        child: Image.asset(banners[itemIndex]),
+                      ),
+              options: CarouselOptions(
+                aspectRatio: 383 / 222,
+                viewportFraction: 0.82,
+                initialPage: 0,
+                enableInfiniteScroll: true,
+                reverse: false,
+                autoPlay: true,
+                autoPlayInterval: const Duration(seconds: 3),
+                autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                autoPlayCurve: Curves.fastOutSlowIn,
+                enlargeCenterPage: true,
+                scrollDirection: Axis.horizontal,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
