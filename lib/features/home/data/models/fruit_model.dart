bbc1b223fc2989c1414bddc9a57e4f5b5126e68e
amrElzohairy <aelzohairy9@gmail.com>
2025-04-17 15:48:36 +0200

    create fruit model.dart

---
+++ lib/features/home/data/models/fruit_model.dart
@@ -0,0 +1,15 @@
+class FruitModel {
+  final String name;
+  final String image;
+  final String price;
+  final num rate;
+  final num rateCount;
+
+  FruitModel({
+    required this.name,
+    required this.image,
+    required this.price,
+    required this.rate,
+    required this.rateCount,
+  });
+}
