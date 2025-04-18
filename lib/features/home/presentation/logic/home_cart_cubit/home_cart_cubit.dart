9e7d3658ee442ddfd7405e77d3e653ad680aac0f
amrElzohairy <aelzohairy9@gmail.com>
2025-04-18 14:17:18 +0200

    add cart logic

--- lib/features/home/presentation/logic/home_cart_cubit/home_cart_cubit.dart
+++ lib/features/home/presentation/logic/home_cart_cubit/home_cart_cubit.dart
@@ -1,8 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_app/features/home/data/models/fruit_model.dart';
 
 part 'home_cart_state.dart';
 
 class HomeCartCubit extends Cubit<HomeCartState> {
   HomeCartCubit() : super(HomeCartInitial());
+  List<FruitModel> cartFruits = [];
+  
+  int get cartCount => cartFruits.length;
+
+  void addToCart(FruitModel fruit) {
+    cartFruits.add(fruit);
+    emit(AddToCart());
+  }
+
+  void removeFromCart(FruitModel fruit) {
+    cartFruits.remove(fruit);
+    emit(RemoveFromCart());
+  }
+
+  void clearCart() {
+    cartFruits.clear();
+    emit(CartEmpty());
+  }
+
+  bool isInCart(FruitModel fruit) {
+    return cartFruits.contains(fruit);
+  }
+
+  void toggleCart(FruitModel fruit) {
+    if (isInCart(fruit)) {
+      removeFromCart(fruit);
+    } else {
+      addToCart(fruit);
+    }
+  }
 }
