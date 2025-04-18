2ee7ea4172f2e84c3f2dcf1e15b273fea6221bb1
amrElzohairy <aelzohairy9@gmail.com>
2025-04-18 15:23:18 +0200

    reafacto app title

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fruits_app/core/utils/app_colors.dart';
 import 'package:fruits_app/features/splash/presentation/views/splash_view.dart';
 
 void main() {
@@ -16,14 +17,12 @@ class FruitsApp extends StatelessWidget {
       designSize: const Size(360, 690),
       minTextAdapt: true,
       splitScreenMode: true,
-      // Use builder only if you need to use library outside ScreenUtilInit context
       builder: (_, child) {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
-          title: 'First Method',
-          // You can use the library anywhere in the app even in theme
+          title: 'Fruits App',
           theme: ThemeData(
-            primarySwatch: Colors.blue,
+            primaryColor: AppColors.primaryColor,
             textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
           ),
           home: child,
